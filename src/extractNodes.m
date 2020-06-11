204b83d17d7577f8d45f40138b06a4a722f1ebfe
Thomas Dehaeze <dehaeze.thomas@gmail.com>
2020-06-11 17:31:00 +0200

    Update index.org

--- src/extractNodes.m
+++ src/extractNodes.m
@@ -56,8 +56,8 @@ while 1
     if nextline < 0, break, end
 
     % Line just before the list of node DOF
-    if contains(nextline, 'NODE') && ...
-           contains(nextline, 'LABEL')
+  if contains(nextline, 'NODE') && ...
+         contains(nextline, 'LABEL')
 
         while 1
             nextline = fgetl(fid);
