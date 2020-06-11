7041e3786c0287d609cfe2cb9dbac18b9a4dac5d
pbrumund-73 <59114255+pbrumund-73@users.noreply.github.com>
2020-06-11 17:19:50 +0200

    Change for ANSYS 2020R1
    String marking beginning of Master DoF changed to "NODE  LABEL"

--- src/extractNodes.m
+++ src/extractNodes.m
@@ -56,7 +56,7 @@ while 1
     if nextline < 0, break, end
 
     % Line just before the list of node DOF
-    if contains(nextline, 'NODE DOF')
+    if contains(nextline, 'NODE  LABEL')
 
         while 1
             nextline = fgetl(fid);
